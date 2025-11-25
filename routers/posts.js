5c72c65e9bde6700a62ec9d2b1bc7007efa43ffa
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-25 12:35:40 +0100

    add "s" to export end of posts
    It were driving me crazy I couldn't activate the server all due to a missing "s"

--- routers/posts.js
+++ routers/posts.js
@@ -2,28 +2,28 @@ const express = require('express')
 const router = express.Router()
 
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.send('show all the posts here')
 })
 
-app.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.send(`Show the post with id: ${req.params.id}`)
 })
 
-app.post('/', (req, res) => {
+router.post('/', (req, res) => {
     res.send('Store a new post here')
 })
 
-app.put('/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     res.send(`Update the post with id: ${req.params.id}`)
 })
 
-app.patch('/:id', (req, res) => {
+router.patch('/:id', (req, res) => {
     res.send(`modify the post with id: ${req.params.id}`)
 })
 
-app.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     res.send(`Delete the post with id: ${req.params.is}`)
 })
 
-module.export = router
+module.exports = router
--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express()
 
 const PORT = 3000
 
+const postRouter = require('./routers/posts')
+
 app.listen(PORT, () => {
     console.log(`Server listenig on port: ${PORT}`);
 
 })
+
+app.use('/api/posts', postRouter)
